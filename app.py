087fb35b3bd4c48ac04e06187933bb8669914e02
TG bot in Python <asliddinchoriyev87@gmail.com>
2024-07-22 16:27:47 +0900

    first commit is done

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+# Title
+st.title("Transport Classification")
+
+#image uploader
+file = st.file_uploader("Upload image", type = ['jpg','jpeg','png','svg'])
+if file:
+    st.image(file)
+
+    #PIL convert
+    img = PILImage.create(file)
+
+    #model
+    model = load_learner('transport_model.pkl')
+
+    #prediction
+    pred, pred_id, probs= model.predict(img)
+    st.success(f'Prediction : {pred}')
+    st.info(f'Probality : {probs[pred_id]*100:.1f}%')
+
+#plotting
+figure = px.bar(x = probs * 100, y = model.dls.vocab)
+st.plotly_chart(figure)
