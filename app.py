604c9669640f49cc34ae5955e6b53b164925a1cd
Asliddin Choriyev <79892973+asliddinchoriev87@users.noreply.github.com>
2024-07-22 17:25:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ if file:
     st.info(f'Probality : {probs[pred_id]*100:.1f}%')
 
     #plotting
-    figure = px.bar(x = probs * 100, y = model.dls.vocab, x_label('Percentage'), y_label('Type'))
+    figure = px.bar(x = probs * 100, y = model.dls.vocab, x_label(str), y_label(str))
     st.plotly_chart(figure)
